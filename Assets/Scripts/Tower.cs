883e06910ddca6043c8920a2706f99fd6097bbd8
tomas96ech <techeverri96@gmail.com>
2025-04-26 21:42:38 -0500

    feat: look at enemies
    Only target on range enemies

--- Assets/Scripts/Tower.cs
+++ Assets/Scripts/Tower.cs
@@ -1,4 +1,5 @@
 
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Tower : MonoBehaviour
@@ -9,13 +10,29 @@ public class Tower : MonoBehaviour
     [SerializeField] private Transform towerHead;
     [SerializeField] private float rotationSpeed;
 
+    [SerializeField] private float attackRange = 1.5f;
+    [SerializeField] private LayerMask enemyMask;
+
     private void Update()
     {
+        if (currentEnemy == null)
+        {
+            currentEnemy = FindRandomEnemyInRange();
+            return;
+        }
+
+        if (Vector3.Distance(currentEnemy.position, transform.position) > attackRange)
+        {
+            currentEnemy = null;
+        }
+
         RotateTowardsEnemy();
     }
 
     private void RotateTowardsEnemy()
     {
+        if (currentEnemy == null) return;
+
         Vector3 direction = currentEnemy.position - towerHead.position;
 
         Quaternion lookDirection = Quaternion.LookRotation(direction);
@@ -23,5 +40,30 @@ public class Tower : MonoBehaviour
         Vector3 rotation = Quaternion.Lerp(towerHead.rotation, lookDirection, rotationSpeed * Time.deltaTime).eulerAngles;
 
         towerHead.rotation = Quaternion.Euler(rotation); 
+    }
+
+    private Transform FindRandomEnemyInRange()
+    {
+        List<Transform> possibleTargets = new List<Transform>();
+        Collider[] enemiesAround = Physics.OverlapSphere(transform.position, attackRange, enemyMask);
+
+        foreach (var enemy in enemiesAround)
+        {
+            possibleTargets.Add(enemy.transform);
+        }
+
+        int randomNumber = Random.Range(0, possibleTargets.Count);
+
+        if (possibleTargets.Count <= 0)
+        {
+            return null;
+        }
+
+        return possibleTargets[randomNumber];
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(transform.position, attackRange);
     }
 }
